279bed32afd8a04bacad44ea75df29352c35e2d3
Milan Gittler <gittlermilan@seznam.cz>
2025-02-03 15:37:41 +0100

    feat(Server-Commands): Change delegete to func

--- Server/Commands/UsersCommand.cs
+++ Server/Commands/UsersCommand.cs
@@ -3,15 +3,21 @@ using Server.Interfaces;
 
 namespace Server.Commands;
 
-public class UsersCommand(ConnectedClientsDelegate connectedClients) : ICommand
+public class UsersCommand : ICommand
 {
     public CommandName Name => CommandName.Users;
     public string Description => "Shows number of connected users";
     public string Usage => "/users";
-    private readonly ConnectedClientsDelegate _connectedClients = connectedClients;
+    private readonly Func<int> _connectedClientsCount;
+
+    public UsersCommand(Func<int> connectedClientsCount)
+    {
+        ArgumentNullException.ThrowIfNull(connectedClientsCount);
+        _connectedClientsCount = connectedClientsCount;
+    }
 
     public string Execute(string[] parameters)
     {
-        return $"Connected users: {_connectedClients()}";
+        return $"Connected users: {_connectedClientsCount?.Invoke()}";
     }
 }
